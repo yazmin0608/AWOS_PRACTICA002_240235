425a5983cba2e1f66dde5a6a7b22bf8cf504b12d
Yazmin Ariana Esquivel Marquez <240235@utxicotepec.edu.mx>
2026-01-20 09:43:50 -0600

    Practica 02: Objetos
    update: Se agregaron los puntos 3, 4 y 5 complemento del Ejercicio 04

--- js/Ejercicio04.js
+++ js/Ejercicio04.js
@@ -46,6 +46,7 @@ console.log(`Datos de la propiedad en RENTA:
         price:7500.00,
         isRentable:true,
         isSaleable:false,
+        isSaleable:true,
         includeServices:["Agua","Luz","Gas"],
         lat:19.4283813,
         long:-99.1020177,
@@ -55,3 +56,124 @@ console.log(`Datos de la propiedad en RENTA:
     };
      //Impresion del objeto en formato tabla
      console.table(property1)
+
+     // 3. Leyendo laspropiedades de un objeto
+     /* Un objeto es un conjunto de variables que abstraen una entidad física o conceptual de la
+     que es significativo almacenar información para el programa o sistema que estemos 
+     desarrollado.
+
+     Para acceder a las propiedades individuales basta con poner el nombre del objeto un "." y el 
+     nombre de la propiedad*/
+
+     console.warn("3. Lectura de Propiedades de un Objeto");
+
+    console.log(`El usuario con id: ${property1.ownerID} esta rentando un: ${property1.name}
+    cual se ddescribe como: ${property1.description} con un costo de: ${property1.price}
+    con los servicio incluidos de: ${property1.includedServices}${property1.isSeleable ? ", actualmente esta a la venta" :
+    "que por el momento no esta a la venta"}`);
+
+    // 4. Modificación de los valores de las propiedades de un objeto
+    /*Para modificar los datos de una variable basta con acceder a la propieddad deseada a
+    modificar y asignarle su nuevo a tráves del signo =*/
+
+    console.warn("4. Modificación de las propiedades de un objeto");
+    property1.price = 8200.00;
+    property1.isSeleable = true;
+    property1.includedServices = ["agua", "luz", "gas", "internet", "seguridad privada"];
+
+
+    console.log(`El usuario con id: ${property1.ownerID} esta rentando un: ${property1.name} el
+    cual se describe como: ${property1.description} por un precio de: ${property1.price} con los 
+    servicio incluidos de: ${property1.includedServices} ${property1.isSeleable ? ", actualmente esta a la venta" :
+    "que por el momento no esta a la venta"}`);
+
+
+    // Destructuración de Objetos (OBJECT DESTRUCTURING)
+    /*JavaScript es un lenguaje dinámico que permite que un objeto previamente definido pueda ser
+    descompuesto en pequeñas nuevas partes (variables) para que estas puedan ser manipuladas segun
+    sea la necessidad*/
+
+    user1 =
+    {
+        userID: 32412,
+        name: "John Doe",
+        email: "John.doe@gamail.com",
+        createdAt: new Date("2015/01/30 23:15:52"),
+        lastLogin: new Date("2026/01/20 08:00:05"),
+        userRole: ("Vandedor"),
+        totalProperties: 12,
+        rating: 9.4,
+    }
+
+     user2 =
+    {
+        userID: 48571,
+        name: "Yazmin Esquivel",
+        email: "yazmin@gamail.com",
+        createdAt: new Date("2024/08/20 15:08:58"),
+        lastLogin: new Date("2026/01/19 23:15:35"),
+        userRole: ("Comprador"),
+        rating: 8.5,
+        lastOperation: "Buscando Departamento para renta"
+    }
+
+    user3 =
+    {
+        userID: 4947,
+        name: "Andrea Acosta",
+        email: "andrea@gamail.com",
+        createdAt: new Date("2025/09/21 15:08:50"),
+        lastLogin: new Date("2026/02/18 20:00:40"),
+        userRole: ("Comprador"),
+        rating: 8.5,
+        lastOperation: "Buscando Departamento para renta"
+    }
+
+     user4 =
+    {
+        userID: 4846,
+        name: "Obed Vargas",
+        email: "obed@gamail.com",
+        createdAt: new Date("2023/09/18 13:00:30"),
+        lastLogin: new Date("2026/02/18 19:00:00"),
+        userRole: ("Comprador"),
+        rating: 8.5,
+        lastOperation: "Buscando Departamento para renta"
+    }
+
+    console.warn("5. Desestructuración de Objetos")
+
+    const buyerBid = (seller, buyer, property, price_bidded) => {
+
+        const {userID: sellerID, email: sellerEmail} = seller;
+        const {userID: buyerID, email: buyerEmail} = buyer;
+        const {price,id} = property;
+
+        const fecha_oferta = new Date();
+
+
+        console.log(`El usuario: ${buyerID} esta realizando un oferta al usuario: ${sellerID}
+            por la cantidad de: ${price} por la propiedad ${id} confecha: ${new Date}`);
+
+        return {
+            buyerID,
+            sellerID,
+            propertyID: id,
+            propertyPrice: price,
+            date_bidded: fecha_oferta,
+            bid_accurrate: Math.round((price_bidded/ price) * 100 *100) / 100 +"%",
+
+        }
+    }
+
+    // Test 1: Ell usuario 2 realiza una oferta de 7500
+    let bid1 = buyerBid(user1, user2, property1,7500);
+    console.log(`La oferta tiene una exactitud del: ${bid1.bid_accurrate}`)
+
+    // Test 2: Ell usuario 3 realiza una oferta de 8200
+    let bid2 = buyerBid(user1, user3, property1,8200);
+    console.log(`La oferta tiene una exactitud del: ${bid2.bid_accurrate}`)
+
+    // Test 3: Ell usuario 4 realiza una oferta de 10000
+    let bid3 = buyerBid(user1, user4, property1,10000);
+    console.log(`La oferta tiene una exactitud del: ${bid3.bid_accurrate}`)
